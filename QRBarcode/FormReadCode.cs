db361199a417c34fd9a66d4f6dc077de7306997f
Verschling <verschline@gmail.com>
2024-07-17 09:31:56 +0300

    Убрал ненужные using

--- QRBarcode/FormReadCode.cs
+++ QRBarcode/FormReadCode.cs
@@ -1,13 +1,6 @@
 ﻿using IronBarCode;
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace QRBarcode
